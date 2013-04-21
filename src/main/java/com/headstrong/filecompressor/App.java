b6ef93eda3c95b3970e6b5fc34f71aadd3e59d1b
Raynard R. Delos Reyes <dranyar@gmail.com>
2013-04-21 20:36:29 +0800

    Initial commit of Maven structured files.

---
+++ src/main/java/com/headstrong/filecompressor/App.java
@@ -0,0 +1,13 @@
+package com.headstrong.filecompressor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
