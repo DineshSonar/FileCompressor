471fb8b318b5ded60d5f9a010bf5f46d00c5b18f
Raynard R. Delos Reyes <dranyar@gmail.com>
2013-04-21 22:22:30 +0800

    --Added .gitignore file. --Refactored FileCompressor classes

---
+++ src/test/java/com/headstrong/filecompressor/FileCompressorTest.java
@@ -0,0 +1,38 @@
+package com.headstrong.filecompressor;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class FileCompressorTest extends TestCase
+{
+	/**
+	 * Create the test case
+	 * 
+	 * @param testName
+	 *            name of the test case
+	 */
+	public FileCompressorTest(String testName)
+	{
+		super(testName);
+	}
+
+	/**
+	 * @return the suite of tests being tested
+	 */
+	public static Test suite()
+	{
+		return new TestSuite(FileCompressorTest.class);
+	}
+
+	/**
+	 * Rigourous Test :-)
+	 */
+	public void testApp()
+	{
+		assertTrue(true);
+	}
+}
